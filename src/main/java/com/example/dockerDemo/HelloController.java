ead54eb5bb27e4ede239a6509bb966c81ad89649
chadipiralla2428 <schadipiralla@gmail.com>
2023-03-22 11:53:47 +0530

    first dockerDemo commit

---
+++ src/main/java/com/example/dockerDemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.dockerDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	@GetMapping("/get")
+	public String helloDemo() {
+		return "welcome to docker container";
+	}
+
+}
